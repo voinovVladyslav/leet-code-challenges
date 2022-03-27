f2b363fff02066f1ec9729e8f56000685a1d47be
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-03-27 20:27:01 +0300

    20  solved

--- validParentheses.py
+++ validParentheses.py
@@ -1,7 +1,21 @@
 def isValid(s: str) -> bool:
-    pass
-
-        
+    if len(s) % 2 == 1:
+        return False
+    prev = ''
     
+    while prev != s and len(s) != 0: 
+        prev = s
+        s = s.replace('()', '--')
+        s = s.replace('[]','--')
+        s = s.replace('{}','--')
 
-print(isValid("()()"))
+        t = s.split("--")
+        s = "".join(t)
+        print(s)
+        
+    if len(s) == 0:
+        return True
+    else:
+        return False
+
+print(isValid("({(())}[[]]())))"))
