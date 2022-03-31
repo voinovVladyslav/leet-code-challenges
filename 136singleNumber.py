8552befba5dd5090d6491c7a771b36afb3a0158e
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-03-31 21:38:27 +0300

    136 solved

---
+++ 136singleNumber.py
@@ -0,0 +1,15 @@
+from wsgiref.util import request_uri
+
+
+def singleNumber(n: list[int]) -> int:
+    n.sort()
+    if len(n) > 1:
+        if n[0] != n[1]:
+            return n[0]
+
+    for i in range(1, len(n)-1):
+        if n[i] != n[i + 1] and n[i] != n[i - 1]:
+            return n[i]
+    return n[-1]
+
+print(singleNumber([1]))
